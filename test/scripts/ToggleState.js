b3b43b863b5cc33dde87d0b5b9fe3592db6f17ce
Otto Streifel <ottost@microsoft.com>
2016-10-03 07:48:27 -0700

    Touch up

--- test/scripts/ToggleState.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    (function (ToggleState) {
-        ToggleState[ToggleState["True"] = 0] = "True";
-        ToggleState[ToggleState["False"] = 1] = "False";
-        ToggleState[ToggleState["Indeterminate"] = 2] = "Indeterminate";
-    })(exports.ToggleState || (exports.ToggleState = {}));
-    var ToggleState = exports.ToggleState;
-});
